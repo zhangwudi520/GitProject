ac5a05ddd0c168aa6a23150bd8015dfb085dd497
ZhangWudi <729474981@qq.com>
2019-10-22 16:29:14 +0800

    day01-水仙花数

--- Day01/02.py
+++ Day01/02.py
@@ -1,4 +1,23 @@
 # -*- coding:utf-8 -*-
 __author__ = 'zdt'
 __date__ = '2019/10/22 15:56'
-import this
+
+
+def main():
+    """
+    求水仙花数
+    //  整除
+    /   除
+    %   取余
+    :return:
+    """
+    for i in range(123, 1000):
+        best = i // 100
+        ten = i // 10 % 10
+        a = i % 10
+        if best ** 3 + ten ** 3 + a ** 3 == i:
+            print(i)
+
+
+if __name__ == '__main__':
+    main()
